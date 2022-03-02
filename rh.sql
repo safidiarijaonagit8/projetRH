99908b75c97b63c2dca87d9b00977c9dacdf2f86
tahinaakjea <akjeachan@gmail.com>
2022-03-02 11:37:54 +0300

    Update rh.sql

--- rh.sql
+++ rh.sql
@@ -1,7 +1,7 @@
-create database Employe
-c\Employe
-create table Employe{
-employeid int serial primary key, 
+create database Employe;
+\c Employe;
+create table Employe(
+employeid int primary key not null, 
 nom varchar(50),
 prenom varchar(50),
 datenaissance date,
@@ -10,48 +10,42 @@ ardesse varchar(50),
 foreign key (idposte) references Poste(posteid),
 dateembauche date,
 foreign key (idtypecontrat) references Typecontrat(typecontratid) 
-};
-INSERT INTO Employe
-INSERT INTO Employe  
-INSERT INTO Employe
-INSERT INTO Employe
-(
-  Column1,
-  Column2,
-  Column3
-)
-VALUES
-(
-  'TEST USER1',
-  'FirstName.LastName',
-  321
 );
-create table Typecontrat{
-typecontratid int serial primary key,
+create table Typecontrat(
+typecontratid int primary key not null,
 typecontra varchar(50),
 dureemois real
-};
-
-create table Indemnite{
-indamniteid int serial primary key,
+);
+create table Indemnite(
+indamniteid int primary key not null,
 foreign key (idposte) references Poste(posteid),
 valeur real
-};
+);
 
-create table Poste{
-posteid int serial primary key,
+create table Poste(
+posteid int primary key not null,
 nom varchar(50),
 foreign key (iddepartement) references Departement(departementid),
 salaire real
-};
+);
 
-create table Departement{
-departementid int serial primary key,
+create table Departement(
+departementid int primary key not null,
 nom varchar(50)
-};
+);
 
-create table Admin{
-adminid int serial primary key,
+create table Admin(
+adminid int primary key not null,
 username varchar(50),
 passwword varchar(50)
-};
+);
+create view as v_detailemploye
+	SELECT*
+	From Employer e join poste p
+	on e.idposte=p.posteid,e.typecontratid=t.idtypecontrat
+
+	join Departement d
+	on d.iddepartement=p.departementid
+	join Indemnite i
+	join typecontra t
+	;
